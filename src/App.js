b296be96bf0333287809124e6fe8c456f2f5986c
manhtieneaut <20200892@eaut.edu.vn>
2023-02-14 18:10:13 +0700

    bài thực hành 3

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CheckAuthen from './components/Checkauthen';
+class App extends Component {
+
+  render() {
+    return (
+      <div className='container'>
+        <CheckAuthen/>
+
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Checkauthen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+class CheckAuthen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            isLoggedIn: false,
+        }
+    }
+    componentDidMount() {
+        const getUserInfo = localStorage.getItem('isLoggedIn');
+        let updatedValue = this.state.isLoggedIn;
+        if (getUserInfo) {
+            updatedValue = true;
+        } else {
+            updatedValue = false;
+        }
+        this.setState({
+            loading: false,
+            isLoggedIn: updatedValue
+        })
+    }
+    render() {
+        return (
+            <div>
+                {this.state.loading && <h3>Loading</h3>}
+                {this.state.isLoggedIn ? (
+                    <div>
+                        <h3>Hello User</h3>
+                        <button>Logout</button>
+                    </div>
+                ) : (
+                    <div>
+                        <h3>Please login</h3>
+                        <button>Login</button>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+export default CheckAuthen;
